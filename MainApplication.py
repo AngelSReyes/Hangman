a7dd0dc779586b76583de4e4b8d0f78d03679899
Angel Reues <shayereyes258@gmail.com>
2021-08-16 17:28:43 +0100

    added MainApplication skelatol file

---
+++ MainApplication.py
@@ -0,0 +1,27 @@
+"""
+Author          : Ben Fisher and Angel Reyes
+Date Created    : 16/08/2021
+Date Modified   : 
+Version         : 0.1
+Description     : Hangman game
+History         : 0.1   : File creation
+"""
+
+#Import required libraries
+
+
+def exampleFunction():
+    """
+    Example function description
+    """
+    exampleVariable = "Hello world!"
+    return exampleVariable
+
+
+def main():
+    exampleVariable = exampleFunction()
+    print(exampleVariable)
+
+
+if __name__ == "__main__":
+    main()
